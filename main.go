447596896ecae2f4d276f559c38443e40a5c1b39
Katherine Hart <kasiahart@yahoo.ca>
2020-04-25 15:56:21 -0400

    Added README and main function

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gorilla/mux"
+
+func main() {
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/signup", signup).Methods("POST")
+	router.HandleFunc("/login", login).Methods("POST")
+	router.HandleFunc("/protected", TokenVerifyMiddleware(ProtectedEndpoint)).Methods("GET")
+}
+
