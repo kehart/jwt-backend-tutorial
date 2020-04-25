00398f4fb6c3bad4462ce596cde15e115f470dd0
Katherine Hart <kasiahart@yahoo.ca>
2020-04-25 19:29:45 -0400

    Implemented login function

--- main.go
+++ main.go
@@ -123,14 +123,53 @@ func GenerateToken(user User) (string, error) {
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-
 	var user User
+	var jwt JWT
+	var error Error
+
 	json.NewDecoder(r.Body).Decode(&user)
+
+	// Validation
+	if user.Email == "" {
+		error.Message = "Email is missing"
+		respondWithError(w, http.StatusBadRequest, error)
+		return
+	}
+	if user.Password == "" {
+		error.Message = "Password is missing"
+		respondWithError(w, http.StatusBadRequest, error)
+		return
+	}
+
+	password := user.Password
+
+	row := db.QueryRow("select * from users where email=$1", user.Email)
+	err := row.Scan(&user.ID, &user.Email, &user.Password); if err != nil {
+		if err == sql.ErrNoRows {
+			error.Message = "The user does not exist"
+			respondWithError(w, http.StatusBadRequest, error)
+			return
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	hashedPassword := user.Password
+
+	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password)); if err != nil {
+		error.Message = "Invalid Password"
+		respondWithError(w, http.StatusUnauthorized, error)
+		return
+	}
 
 	token, err := GenerateToken(user); if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(token, err)
+
+	w.WriteHeader(http.StatusOK)
+	jwt.Token = token
+
+	responseJSON(w, jwt)
 }
 
 func protectedEndpoint(w http.ResponseWriter, r *http.Request) {
