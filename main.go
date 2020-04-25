97d056aba791baed4faed736c7137c624191f22d
Katherine Hart <kasiahart@yahoo.ca>
2020-04-25 16:02:13 -0400

    Implemented dummies of handler functions

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
 
 func main() {
 
@@ -8,6 +13,26 @@ func main() {
 
 	router.HandleFunc("/signup", signup).Methods("POST")
 	router.HandleFunc("/login", login).Methods("POST")
-	router.HandleFunc("/protected", TokenVerifyMiddleware(ProtectedEndpoint)).Methods("GET")
+	router.HandleFunc("/protected", TokenVerifyMiddleware(protectedEndpoint)).Methods("GET")
+
+	log.Println("Listening on port 8000")
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("signup invoked")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("login invoked")
+}
+
+func protectedEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("protectedEndpoint invoked")
+}
+
+func TokenVerifyMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return nil
+}
+
 
