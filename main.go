11ccd5a43110504f5741fd2ccf7f475cc113000b
Katherine Hart <kasiahart@yahoo.ca>
2020-04-27 15:25:39 -0400

    Refactored into controllers

--- main.go
+++ main.go
@@ -2,20 +2,14 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
-	"fmt"
-	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
+	"github.com/jwt-backend-tutorial/controllers"
 	"github.com/jwt-backend-tutorial/driver"
-	"github.com/jwt-backend-tutorial/models"
-	"github.com/jwt-backend-tutorial/utils"
 	"github.com/subosito/gotenv"
-	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
-	"os"
-	"strings"
 )
+
 var db *sql.DB
 
 func init() {
@@ -25,156 +19,13 @@ func init() {
 func main() {
 
 	db = driver.ConnectDB()
+	controller := controllers.Controller{}
 	router := mux.NewRouter()
 
-	router.HandleFunc("/signup", signup).Methods("POST")
-	router.HandleFunc("/login", login).Methods("POST")
-	router.HandleFunc("/protected", TokenVerifyMiddleware(protectedEndpoint)).Methods("GET")
+	router.HandleFunc("/signup", controller.Signup(db)).Methods("POST")
+	router.HandleFunc("/login", controller.Login(db)).Methods("POST")
+	router.HandleFunc("/protected", controller.TokenVerifyMiddleware(controller.ProtectedEndpoint())).Methods("GET")
 
 	log.Println("Listening on port 8000")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
-
-
-func signup(w http.ResponseWriter, r *http.Request) {
-	var user models.User
-	var error models.Error
-	json.NewDecoder(r.Body).Decode(&user)
-
-	//spew.Dump(user)
-	if user.Email == "" {
-		error.Message = "Email is missing"
-		utils.RespondWithError(w, http.StatusBadRequest, error)
-		return
-	}
-	if user.Password == "" {
-		error.Message = "Password is missing"
-		utils.RespondWithError(w, http.StatusBadRequest, error)
-		return
-	}
-
-	// Encrypt the password
-	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10); if err != nil {
-		log.Fatal(err)
-	}
-	user.Password = string(hash)
-
-	// Insert into DB
-	stmt := "insert into users (email, password) values($1, $2) RETURNING id;"
-	err = db.QueryRow(stmt, user.Email, user.Password).Scan(&user.ID); if err != nil {// since query returns id
-		error.Message = "Server error"
-		utils.RespondWithError(w, http.StatusInternalServerError, error)
-		return
-	}
-	user.Password = ""
-	utils.ResponseJSON(w, user)
-}
-
-func GenerateToken(user models.User) (string, error) {
-	secret := os.Getenv("SECRET") // could be anything
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"email": user.Email,
-		"iss": "course",
-	})
-
-	tokenString, err := token.SignedString([]byte(secret)); if err != nil {
-		log.Fatal(err)
-	}
-
-	return tokenString, nil
-}
-
-func login(w http.ResponseWriter, r *http.Request) {
-	var user models.User
-	var jwt models.JWT
-	var error models.Error
-
-	json.NewDecoder(r.Body).Decode(&user)
-
-	// Validation
-	if user.Email == "" {
-		error.Message = "Email is missing"
-		utils.RespondWithError(w, http.StatusBadRequest, error)
-		return
-	}
-	if user.Password == "" {
-		error.Message = "Password is missing"
-		utils.RespondWithError(w, http.StatusBadRequest, error)
-		return
-	}
-
-	password := user.Password
-
-	row := db.QueryRow("select * from users where email=$1", user.Email)
-	err := row.Scan(&user.ID, &user.Email, &user.Password); if err != nil {
-		if err == sql.ErrNoRows {
-			error.Message = "The user does not exist"
-			utils.RespondWithError(w, http.StatusBadRequest, error)
-			return
-		} else {
-			log.Fatal(err)
-		}
-	}
-
-	hashedPassword := user.Password
-
-	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password)); if err != nil {
-		error.Message = "Invalid Password"
-		utils.RespondWithError(w, http.StatusUnauthorized, error)
-		return
-	}
-
-	token, err := GenerateToken(user); if err != nil {
-		log.Fatal(err)
-	}
-
-	w.WriteHeader(http.StatusOK)
-	jwt.Token = token
-
-	utils.ResponseJSON(w, jwt)
-}
-
-func protectedEndpoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("protectedEndpoint invoked")
-}
-
-func TokenVerifyMiddleware(next http.HandlerFunc) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var errorObject models.Error
-		authHeader := r.Header.Get("Authorization") // returns "KEY VAL"
-		bearerToken := strings.Split(authHeader, " ")
-
-		if len(bearerToken) == 2 {
-			authToken := bearerToken[1]
-
-			token, error := jwt.Parse(authToken, func(token *jwt.Token) (interface{}, error) {
-				//spew.Dump(token)
-
-				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok { // verifies the algorithm is ok
-					return nil, fmt.Errorf("there was an error")
-				}
-
-				return []byte(os.Getenv("SECRET")), nil
-			}); if error != nil {
-				errorObject.Message = error.Error()
-				utils.RespondWithError(w, http.StatusUnauthorized, errorObject)
-				return
-			}
-
-			if token.Valid {
-				next.ServeHTTP(w, r)
-			} else {
-				errorObject.Message = error.Error()
-				utils.RespondWithError(w, http.StatusUnauthorized, errorObject)
-				return
-			}
-		} else {
-			errorObject.Message = "Invalid token"
-			utils.RespondWithError(w, http.StatusUnauthorized, errorObject)
-			return
-		}
-	})
-}
-
-
--- utils/utils.go
+++ utils/utils.go
@@ -2,8 +2,11 @@ package utils
 
 import (
 	"encoding/json"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/jwt-backend-tutorial/models"
+	"log"
 	"net/http"
+	"os"
 )
 
 func RespondWithError(w http.ResponseWriter, status int, error models.Error) {
@@ -14,4 +17,19 @@ func RespondWithError(w http.ResponseWriter, status int, error models.Error) {
 func ResponseJSON(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(data)
+}
+
+func GenerateToken(user models.User) (string, error) {
+	secret := os.Getenv("SECRET")
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email": user.Email,
+		"iss": "course",
+	})
+
+	tokenString, err := token.SignedString([]byte(secret)); if err != nil {
+		log.Fatal(err)
+	}
+
+	return tokenString, nil
 }
